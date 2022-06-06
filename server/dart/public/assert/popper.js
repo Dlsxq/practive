6d3a0dd96ef53e8a258cdfab5de06ab193e1fff6
lsxqer <hxy90667@163.com>
2022-06-06 11:37:48 +0800

    frame

--- server/dart/public/assert/popper.js
+++
@@ -1,6 +0,0 @@
-
-
-const $modal = (ele) => {
-  console.log(ele);
-  return new bootstrap.Modal(ele);
-}
--- server/dart/web-serve/lib/serve.dart
+++
@@ -1,8 +0,0 @@
-
-library serve;
-
-export "./src/ctor.dart" show Serve;
-export "./src/router.dart" show Router, RouterHandler;
-export "./src/static.dart" show StaticServe;
-export "./src/reader.dart" show ReadBuilder;
-export "./src/write.dart" show WriteBuilder;
