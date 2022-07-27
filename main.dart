ba14d8707a5fdf7764e82d403c6f6a912e08e61b
Hxy <houxueyou@induschain.cn>
2022-07-27 17:28:25 +0800

    服务接口传输js

--- main.dart
+++ main.dart
@@ -1,6 +1,30 @@
-
-
+import "dart:io";
+import 'package:serve/serve.dart';
 
 void main(List<String> args) {
-  print("object, dart");
+  var router = Router();
+
+  var server = Serve();
+
+  router.get("/", (reader, writer) {
+    writer.html(File("./ttt.html").readAsStringSync());
+  });
+
+  router.get("/js", (reader, writer) {
+
+    var i = reader.query["index"];
+
+
+    writer.headers.contentType = ContentType("text", "javascript");
+
+    writer.write("""
+
+    globalRecord = {foo: ${i}};
+  
+""");
+  });
+
+  server
+    ..use(router)
+    ..listen(4096);
 }
